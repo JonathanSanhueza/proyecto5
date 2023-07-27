29d0fc03323bc0eb8599ca99b5f15d447843b570
Jonathan Sanhueza <jonathan.sanhueza.c@gmail.com>
2023-07-27 02:33:47 -0400

    carrito de compras

---
+++ src/contexts/ShoppingCart/ShoppingCartState.jsx
@@ -0,0 +1,42 @@
+import ShoppingCartContext from "./ShoppingCartContext"
+import {useShoppingCartReducer} from "./ShoppingCartReducer.jsx"
+
+const ShoppingCartState=(props)=>{
+    const initialState={
+        products: null
+    }
+    const [globalState, dispatch]= useShoppingCartReducer(
+        initialState)
+
+    const addProduct=(product)=>{
+        dispatch({
+            type:"AGREGAR_PRODUCTO",
+            payload:{product}
+        })
+    }
+
+    const removeProduct=(productId)=>{
+        dispatch({
+            type:"QUITAR_PRODUCTO",
+            payload:{productId}
+        })
+    }
+    const getProducts=()=>{
+        dispatch({
+            type:"OBTENER_PRODUCTO"
+        })
+    }
+    return(
+        <ShoppingCartContext.Provider value={{
+            ...globalState,
+            addProduct,
+            removeProduct,
+            getProducts
+        }} displayName="ShoppingCartContext">
+            {props.children}
+        </ShoppingCartContext.Provider>
+    )
+
+}
+
+export default ShoppingCartState
