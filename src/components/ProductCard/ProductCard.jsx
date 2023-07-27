f8baf51b317fb77967292e49c4a52584e63b406e
Jonathan Sanhueza <jonathan.sanhueza.c@gmail.com>
2023-07-27 02:33:34 -0400

    modificacion producto para base de datos

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react"
 import { Card, Button } from "react-bootstrap"
+import ShoppingCartContext from "../../contexts/ShoppingCart/ShoppingCartContext.jsx"
 
 export const ProductCard = ({product, productViewPath})=>{
+    const shoppingCartCtx=useContext(ShoppingCartContext)
+    const{addProduct}=shoppingCartCtx
     return (
     <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={product.imageUrl} />
@@ -11,10 +15,12 @@ export const ProductCard = ({product, productViewPath})=>{
             </Card.Text>
         </Card.Body>
         <Card.Body className="text-center">
-            <Card.Text className="text-center">{product.price}
+            <Card.Text className="text-center">${product.price} USD
             </Card.Text>
             <Button variant="secondary" href={productViewPath}>Ver</Button>
-            <Button variant="secondary">Agregar</Button>
+            <Button variant="secondary" onClick={()=>{
+                addProduct(product)
+            }}>Agregar</Button>
         </Card.Body>
     </Card>
     )
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,67 +1,88 @@
-import { Button, Card, Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import { ProductCard } from "../../components/ProductCard/ProductCard"
+import UserContext from "../../contexts/users/UserContext"
+import { useContext, useEffect, useState } from "react"
+import axiosClient from "../../config/axios"
+
 export const Products = ()=> {
-    const products=[{
-        id:"1",
-        title:"producto 1",
-        description:"descripcion",
-        price:69990,
-        imageUrl:"https://i.ibb.co/3mfvVrV/EM01010228-1.jpg"
-    },
-    {
-        id:"2",
-        title:"producto 2",
-        description:"descripcion",
-        price:69990,
-        imageUrl:"https://i.ibb.co/dmP50KS/FN01010152-1.jpg"
-    },
-    {
-        id:"3",
-        title:"producto 3",
-        description:"descripcion",
-        price:69990,
-        imageUrl:"https://i.ibb.co/FBdv9bs/JD01020032-1.jpg"
-    },
-    {
-        id:"4",
-        title:"producto 4",
-        description:"descripcion",
-        price:69990,
-        imageUrl:"https://i.ibb.co/HqL7qkR/JD01020029-1.jpg"
-    },
-    {
-        id:"5",
-        title:"producto 5",
-        description:"descripcion",
-        price:69990,
-        imageUrl:"https://i.ibb.co/XpDFFDx/JD01030003-1.jpg"
-    },
-    {
-        id:"6",
-        title:"producto 6",
-        description:"descripcion",
-        price:69990,
-        imageUrl:"https://i.ibb.co/Bc0V2Zg/JD01050022.jpg"
-    },
-    {
-        id:"7",
-        title:"producto 7",
-        description:"descripcion",
-        price:69990,
-        imageUrl:"https://i.ibb.co/vP3ZBYz/JD01020031-1.jpg"
-    }
-]
+    const userCtx = useContext (UserContext)
+    const {user} = userCtx
+    const[products, setProducts]= useState([])
+    
+    //const products=[{
+   //     id:"1",
+   //     title:"producto 1",
+   //     description:"descripcion",
+   //     price:69990,
+  //      imageUrl:"https://i.ibb.co/3mfvVrV/EM01010228-1.jpg"
+   // },
+    //{
+  //      id:"2",
+   //     title:"producto 2",
+   //     description:"descripcion",
+   //     price:69990,
+   //     imageUrl:"https://i.ibb.co/dmP50KS/FN01010152-1.jpg"
+   // },
+   // {
+  //      id:"3",
+    //    title:"producto 3",
+    //    description:"descripcion",
+   //     price:69990,
+   //     imageUrl:"https://i.ibb.co/FBdv9bs/JD01020032-1.jpg"
+   // },
+   // {
+   //     id:"4",
+   //     title:"producto 4",
+   //     description:"descripcion",
+   //     price:69990,
+   //     imageUrl:"https://i.ibb.co/HqL7qkR/JD01020029-1.jpg"
+   // },
+   // {
+   //     id:"5",
+  //      title:"producto 5",
+   //     description:"descripcion",
+   //     price:69990,
+   //     imageUrl:"https://i.ibb.co/XpDFFDx/JD01030003-1.jpg"
+   // },
+  //  {
+   //     id:"6",
+   //     title:"producto 6",
+   //     description:"descripcion",
+   //     price:69990,
+   //     imageUrl:"https://i.ibb.co/Bc0V2Zg/JD01050022.jpg"
+  //  },
+   // {
+   //     id:"7",
+   //     title:"producto 7",
+   //     description:"descripcion",
+  //      price:69990,
+  //      imageUrl:"https://i.ibb.co/vP3ZBYz/JD01020031-1.jpg"
+//    }]
+
+    useEffect(()=>{
+        const getProductsFromDB= async()=>{
+            const axiosRes = await axiosClient.get('/products')
+            setProducts(axiosRes.data)
+        }
+        try{
+            getProductsFromDB()
+        }catch(error){
+                console.error(error)
+                alert(error.message)
+            
+        }
+    },[])
     return( 
         <Container fluid>
             <Row>
                 <Col>
-                <h1>Catálogo de productos</h1>
+                <h1>Catálogo de productos {user?.fullName|| "invitado"}</h1>
                 </Col>
             </Row>
             <Row>
                 {products.map(product =>{
-            return<Col key={product.id}>
-                <ProductCard product={ product} productViewPath={`/products/${ product.id}`}></ProductCard>
+            return<Col key={product._id}>
+                <ProductCard product={ product} productViewPath={`/products/${ product._id}`}></ProductCard>
             </Col>
         })
         }
